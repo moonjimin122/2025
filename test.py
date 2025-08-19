43bcd96bec148a477dc023afb482a090fae010cd
moonjimin122 <munjimin122@gmail.com>
2025-08-20 01:09:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -30,6 +30,60 @@ recommendations = {
              "이미지": "https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg"}
         ]
     },
+    "판타지": {
+        "책": [
+            {"제목": "해리 포터와 마법사의 돌", 
+             "줄거리": "소년 마법사 해리 포터가 호그와트에 입학하면서 시작되는 모험.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/6b/HP1_cover.jpg"},
+            {"제목": "반지의 제왕: 반지 원정대", 
+             "줄거리": "절대 반지를 파괴하기 위해 떠나는 모험과 우정의 이야기.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/8/8e/The_Fellowship_of_the_Ring_cover.gif"},
+            {"제목": "나니아 연대기", 
+             "줄거리": "옷장 속 판타지 세계 나니아에서의 전투와 모험.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_box_set_cover.jpg"}
+        ],
+        "영화": [
+            {"제목": "반지의 제왕: 반지 원정대", 
+             "줄거리": "절대 반지를 없애기 위한 원정이 시작된다.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/0/0c/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_%282001%29.jpg"},
+            {"제목": "해리 포터와 마법사의 돌", 
+             "줄거리": "마법사의 세계에 입문한 해리의 첫 모험.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/6b/HP1_cover.jpg"},
+            {"제목": "나니아 연대기: 사자, 마녀 그리고 옷장", 
+             "줄거리": "네 남매가 옷장 속 세계 나니아에서 대모험을 펼친다.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_The_Lion%2C_the_Witch_and_the_Wardrobe_poster.jpg"},
+            {"제목": "판의 미로: 오필리아와 세 개의 열쇠", 
+             "줄거리": "스페인 내전 후 소녀가 환상 세계와 현실 사이를 넘나드는 이야기.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/6f/Pans_labyrinth.jpg"}
+        ]
+    },
+    "스릴러": {
+        "책": [
+            {"제목": "다 빈치 코드", 
+             "줄거리": "암호와 미스터리를 풀어가는 종교적 스릴러.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/6b/DaVinciCode.jpg"},
+            {"제목": "드래곤 타투를 한 소녀", 
+             "줄거리": "사라진 소녀의 비밀을 추적하는 범죄 스릴러.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/0/0c/The_Girl_with_the_Dragon_Tattoo_Poster.jpg"},
+            {"제목": "셜록 홈즈의 모험", 
+             "줄거리": "명탐정 셜록 홈즈의 추리와 스릴 넘치는 사건들.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/commons/8/8d/Sherlock_Holmes_Adventures.jpg"}
+        ],
+        "영화": [
+            {"제목": "세븐", 
+             "줄거리": "7대 죄악을 모티브로 한 연쇄 살인 사건 추적.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/68/Seven_%28movie%29_poster.jpg"},
+            {"제목": "셔터 아일랜드", 
+             "줄거리": "고립된 정신병원에서 벌어지는 미스터리 스릴러.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg"},
+            {"제목": "메멘토", 
+             "줄거리": "단기 기억 상실증 환자의 복수극을 그린 독특한 스릴러.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg"},
+            {"제목": "올드보이", 
+             "줄거리": "15년간 감금되었던 남자의 충격적 복수극.", 
+             "이미지": "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"}
+        ]
+    },
     "SF": {
         "책": [
             {"제목": "듄", 
